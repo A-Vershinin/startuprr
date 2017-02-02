c808ad7b4b6f0741e47b97fa2c6c24815ce59a44
Anatoliy Vershinin <cata07@yandex.ru>
2017-02-02 12:02:26 +0200

    add offer-section

--- app/js/common.js
+++ app/js/common.js
@@ -9,7 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.classList.remove("no-js");
       }
     };
+    //menu
+    mobileMenu();
+    function mobileMenu() {
+      var menuMain = document.querySelector(".menu"),
+          menuToggle = document.querySelector(".menu__toggle");
+      menuMain.classList.remove("menu--nojs");
 
+      menuToggle.addEventListener("click", function() {
+        if (menuMain.classList.contains("menu--closed")) {
+          menuMain.classList.remove("menu--closed");
+          menuMain.classList.add("menu--opened");
+        } else {
+          menuMain.classList.add("menu--closed");
+          menuMain.classList.remove("menu--opened");
+        }
+      });
+    }
 
     console.log("JS active");
 
